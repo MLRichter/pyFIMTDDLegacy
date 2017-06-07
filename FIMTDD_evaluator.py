abaf0d8044b035ac8cfdc42ee75055b7600c8075
JuAutz <jautz@uos.de>
2017-06-07 15:02:29 +0200

    Added a test using flight data

--- FIMTDD_evaluator.py
+++ FIMTDD_evaluator.py
@@ -9,6 +9,7 @@ import itertools
 import time
 from multiprocessing import Pool
 import progressbar as pb
+import os
 
 counter = 0
 def abalone_test(paramlist,show,val):
@@ -21,7 +22,6 @@ def abalone_test(paramlist,show,val):
     with open( "abalone.data", 'rt') as abalonefile:
         for row in abalonefile:
             row=row.rstrip().split(',')
-
             target=float(row[-1])
             if row[0]=="M":
                 numgender=1.
@@ -53,6 +53,78 @@ def abalone_test(paramlist,show,val):
             #plt.show()
             f.clear()
         return [cumLossgd,cumLossls,val,paramlist]
+
+
+
+
+def flightdata_test(paramlist,show,val):
+    fimtgd = FIMTGD(gamma=paramlist[0], n_min=paramlist[1], alpha=[2], threshold=paramlist[3], learn=paramlist[4])
+    fimtls = FIMTLS(gamma=paramlist[0], n_min=paramlist[1], alpha=[2], threshold=paramlist[3], learn=paramlist[4])
+    cumLossgd = [0]
+    cumLossls = [0]
+    symbolDict={}
+    symbolCounter=0.
+    directory=os.fsencode("Flight Data")
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            with open(os.path.join(root,file), 'rt') as flightdata:
+
+                try:
+                    next(flightdata)
+                    for row in flightdata:
+                        row=row.rstrip().split(',')
+
+                        #input=row[0:3,5,7,8,9,11,16,17,18,23]
+                        if not "NA" in row[0:4]+[row[5]]+row[7:10]+[row[11]]+row[16:19]+ [row[23]] and not "NA" in row [14]:
+                            target = float(row[14])
+                            input=[float(x) for x in row[0:4]+[row[5]]+[row[7]]]
+                            if not row[8] in symbolDict.keys():
+                                symbolDict[row[8]]=symbolCounter
+                                symbolCounter=symbolCounter+1.
+                            input.append(symbolDict[row[8]])
+                            if not row[9] in symbolDict.keys():
+                                symbolDict[row[9]]=symbolCounter
+                                symboprilCounter=symbolCounter+1.
+                            input.append(symbolDict[row[9]])
+
+                            input.append(float(row[11]))
+                            if not row[16] in symbolDict.keys():
+                                symbolDict[row[16]]=symbolCounter
+                                symbolCounter=symbolCounter+1.
+                            input.append(symbolDict[row[16]])
+                            if not row[17] in symbolDict.keys():
+                                symbolDict[row[17]]=symbolCounter
+                                symbolCounter=symbolCounter+1.
+                            input.append(symbolDict[row[17]])
+                            input.append(float(18))
+                            input.append(float(row[23]))
+                            #print ("Input: " +str(input))
+                except(UnicodeDecodeError):
+                    continue
+
+                cumLossgd.append(cumLossgd[-1] + np.fabs(target - fimtgd.eval_and_learn(np.array(input), target)))
+                cumLossls.append(cumLossls[-1] + np.fabs(target - fimtls.eval_and_learn(np.array(input), target)))
+
+
+    if show:
+        f=plt.figure()
+        plt.plot(cumLossgd[1:], label="Gradient Descent Loss")
+        f.hold(True)
+        plt.plot(cumLossls[1:], label="Filter Loss")
+       #avglossgd=np.array([cumLossgd[-1]/len(cumLossgd)]*len(cumLossgd))
+        #plt.plot(avglossgd,label="Average GD Loss")
+        #plt.plot([cumLossls[-1]/len(cumLossls)]*len(cumLossls), label="Average Filter Loss")
+        plt.title("CumLoss Ratio:"+str(min(cumLossgd[-1],cumLossls[-1])/max(cumLossgd[-1],cumLossls[-1])))
+        plt.legend()
+        figname="g"+str(paramlist[0])+"_nmin"+str(paramlist[1])+"_al"+str(paramlist[2])+"_thr"+str(paramlist[3])\
+                + "_lr"+str(paramlist[4])+".png"
+        plt.savefig(figname)
+        #plt.show()
+        f.clear()
+    return [cumLossgd,cumLossls,val,paramlist]
+
+
+
 
 def callback_func(list):
     global result_list
@@ -130,6 +202,10 @@ if __name__ == '__main__':
         alphalist = [0.05]
         thresholdlist = [50]
         learnlist = [0.01]
+
+    if(True): #for singular test, set this to true
+        for paramlist in itertools.product(gammalist, n_minlist, alphalist, thresholdlist, learnlist):
+            print (flightdata_test(paramlist,True,12))
     minparamgd=[]
     minvalgd=np.inf
     minparamls=[]
@@ -142,7 +218,7 @@ if __name__ == '__main__':
     c = 0
     bar = pb.ProgressBar(max_value=numberoftests)
     for paramlist in itertools.product(gammalist, n_minlist, alphalist, thresholdlist, learnlist):
-        pool.apply_async(func=abalone_test,args=(paramlist,False,c),callback=callback_func)
+        pool.apply_async(func=flightdata_test,args=(paramlist,False,c),callback=callback_func)
         c = c+1
     pool.close()
     pool.join()
