c5e0e8907b18ccc23290ab146129ce5a2d4c800d
Cypher42 <matrichter@uos.de>
2017-06-21 16:43:46 +0200

    added kielt test

--- FIMTDD_evaluator.py
+++ FIMTDD_evaluator.py
@@ -19,6 +19,87 @@ import sys
 from DataGenerator import *
 sys.setrecursionlimit(100000)
 
+def get_Kiel_data():
+    print("setting up data...")
+    data = list()
+    with open('KielClean.csv', 'r') as fp:
+        w_day = 1.0
+        prev_day = -1.0
+        prev_time = 0.0
+        r = csv.reader(fp, delimiter=',')
+        data = list()
+        for i in r:
+            row = list()
+            for j in i:
+                row.append(float(j))
+            if row == []:
+                continue
+            data.append(row)
+    data = np.array(data)
+    new_data = list()
+    # print len(data)
+    simulationSteps = len(data) - 100
+    print("data loaded in RAM")
+    # print simulationSteps
+    for idx in range(simulationSteps):
+        new_data.append(np.hstack((data[idx][5], data[idx + 48][5], data[idx + 72][5],
+                                   data[idx + 84][5], data[idx + 90][5], data[idx + 93][5],
+                                   data[idx + 94][5], data[idx + 95][5], data[idx + 96][3:])))
+    print("data prepared for processing")
+    data = np.array(new_data)
+    simulationSteps = len(data)
+    return data
+    s#elf.bar = progressbar.ProgressBar(max_value=simulationSteps)
+
+def Kiel_Test(paramlist,show,val):
+    fimtgd=FIMTGD(gamma=paramlist[0], n_min = paramlist[1], alpha=paramlist[2], threshold=paramlist[3], learn=paramlist[4])
+    fimtls=FIMTLS(gamma=paramlist[0], n_min = paramlist[1], alpha=paramlist[2], threshold=paramlist[3], learn=paramlist[4])
+    gfimtls=gFIMTLS(gamma=paramlist[0], n_min = paramlist[1], alpha=paramlist[2], threshold=paramlist[3], learn=paramlist[5])
+    cumLossgd  =[0]
+    cumLossls  =[0]
+    cumLossgls =[0]
+
+    if True:
+        data = get_Kiel_data()
+        c = 0
+        for i in range(100000):
+            c += 1
+            print(str(c)+'/'+str(100000))
+            input = data[i][8:10]
+            #target = data[1][i] + (np.random.uniform() - 0.5) * 0.2
+            target = data[i][10]
+
+            if i > -1:
+                cumLossgd.append(cumLossgd[-1] + np.fabs(target - fimtgd.eval_and_learn(np.array(input), target)))
+                cumLossls.append(cumLossls[-1] + np.fabs(target - fimtls.eval_and_learn(np.array(input), target)))
+                cumLossgls.append(cumLossgls[-1] + np.fabs(target - gfimtls.eval_and_learn(np.array(input), target)))
+            else:
+                #warm start
+                fimtgd.eval_and_learn(np.array(input), target)
+                fimtls.eval_and_learn(np.array(input), target)
+                gfimtls.eval_and_learn(np.array(input), target)
+            #plt.scatter(x=x,y=y)
+            #plt.show()
+            if show:
+                f=plt.figure()
+                plt.plot(cumLossgd[1:], label="Gradient Descent Loss")
+                f.hold(True)
+                plt.plot(cumLossls[1:], label="Filter Loss")
+               #avglossgd=np.array([cumLossgd[-1]/len(cumLossgd)]*len(cumLossgd))
+                #plt.plot(avglossgd,label="Average GD Loss")
+                #plt.plot([cumLossls[-1]/len(cumLossls)]*len(cumLossls), label="Average Filter Loss")
+                plt.title("CumLoss Ratio:"+str(min(cumLossgd[-1],cumLossls[-1])/max(cumLossgd[-1],cumLossls[-1])))
+                plt.legend()
+                figname="g"+str(paramlist[0])+"_nmin"+str(paramlist[1])+"_al"+str(paramlist[2])+"_thr"+str(paramlist[3])\
+                        + "_lr"+str(paramlist[4])+".png"
+                plt.savefig(figname)
+                #plt.show()
+                f.clear()
+            #print(i)
+            #print(fimtgd.count_leaves())
+            #print(fimtgd.count_nodes())
+        return [cumLossgd,cumLossls,cumLossgls,val,paramlist]
+
 def test2d(paramlist,show,val):
     #print(val)
     #print(paramlist)
@@ -462,18 +543,18 @@ if __name__ == '__main__':
         #greedlist=[2,5,50,100]
 
         gammalist=[0.05,0.5,1.0]
-        n_minlist=[5,10,15,35]#np.arange(1,1000,50)
+        n_minlist=[96,96*2,96*4,96*8]#np.arange(1,1000,50)
         alphalist=[0.001]
         thresholdlist= [5,15,100]
         learnlist=[0.01,0.025,0.05]
         greedlist=[1,5,50]
     else:
-        gammalist = [1.0]
-        n_minlist = [35]
-        alphalist = [0.2854]
-        thresholdlist = [100]
+        gammalist = [0.01,0.05,1.0]
+        n_minlist = [96]
+        alphalist = [0.001]
+        thresholdlist = [15]
         learnlist = [0.05]
-        greedlist = [2]
+        greedlist = [5]
 
 
 
@@ -502,18 +583,19 @@ if __name__ == '__main__':
             paramlist = list(paramlist)
             idx = learnlist.index(paramlist[-1])
             paramlist.append(greedlist[idx])
-            pool.apply_async(func=sine_test,args=(paramlist,False,c),callback=callback_func)
-            #callback_func(test2d(paramlist,False,c))
+            pool.apply_async(func=Kiel_Test,args=(paramlist,False,c),callback=callback_func)
+            #callback_func(Kiel_Test(paramlist,False,c))
             c = c+1
     pool.close()
     pool.join()
-    print('Proceses Finished')
+    s0 = ('Proceses Finished for Kiel Dataset:')
     #print(result_list)
     minvalgd, minparamgd, minvalls, minparamls,minvalgls,minparamgls = find_max(result_list)
-    print("Optimal GD: "+ str(minparamgd)+ " with " + str(minvalgd))
-    print("Optimal LS: "+ str(minparamls)+ " with " + str(minvalls))
-    print("Optimal gLS: " + str(minparamgls) + " with " + str(minvalgls))
-
+    s1 = ("Optimal GD: "+ str(minparamgd)+ " with " + str(minvalgd))
+    s2 = ("Optimal LS: "+ str(minparamls)+ " with " + str(minvalls))
+    s3 = ("Optimal gLS: " + str(minparamgls) + " with " + str(minvalgls))
+    with open('results.txt','a+') as fp:
+        fp.write(s0+'\n'+s1+'\n'+s2+'\n'+s3+'\n\n')
     f=plt.figure()
     plt.plot(c_loss_gd[1:], label="FIMTGD")
     f.hold(True)
